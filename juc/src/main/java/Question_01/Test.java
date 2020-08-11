4c8a582f83b842c4427ecb7b103847cef7670bf0
luvnpce <dannyzoujx@gmail.com>
2020-08-11 16:41:27 -0700

    remove test codes

--- juc/src/main/java/Question_01/Test.java
+++ juc/src/main/java/Question_01/Test.java
@@ -3,22 +3,7 @@ package Question_01;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class Test {
-    public static void main(String[] args) {
-//        long a = 1,b = 1;
-//        int result = 300549;
-//        // a小于1000
-//        while (a < 1000) {
-//            if (((a * (a + 1) / 2) - 2 * b - (2 * b - 1)) == 300549) {
-//                System.out.println(a + "," + b);
-//                a = a + 1;
-//                b = 1;
-//            } else if (a > b) {
-//                b = b + 1;
-//            } else {
-//                a = a + 1;
-//                b = 1;
-//            }
-//        }
+    public static void main(String[] args) throws Throwable {
         Test test = new Test();
         ReentrantLock lock = new ReentrantLock();
         Thread t1 = new Thread(() -> {
