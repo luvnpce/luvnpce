a695a3d4c1328d0ac6fdea62819ffe756cb29f98
luvnpce <dannyzoujx@gmail.com>
2020-10-19 21:19:24 -0700

    CopyListWithRandom

---
+++ algo/src/main/java/datastructure/linkedlist/CopyListWithRandom.java
@@ -0,0 +1,105 @@
+package datastructure.linkedlist;
+
+/**
+ * 有一个链表，里面的节点除了next以外还有一个rand
+ * rand会指向链表里任意一个节点（也有可能是Null）
+ *
+ * 要求复制出同样的一个链表出来
+ */
+public class CopyListWithRandom {
+
+    public static class Node {
+        public int value;
+        public Node next;
+        public Node rand;
+
+        public Node(int data) {
+            this.value = data;
+        }
+    }
+
+    public static void main(String[] args) {
+        Node head = null;
+        Node res1 = null;
+
+        head = new Node(1);
+        head.next = new Node(2);
+        head.next.next = new Node(3);
+        head.next.next.next = new Node(4);
+        head.next.next.next.next = new Node(5);
+        head.next.next.next.next.next = new Node(6);
+
+        head.rand = head.next.next.next.next.next; // 1 -> 6
+        head.next.rand = head.next.next.next.next.next; // 2 -> 6
+        head.next.next.rand = head.next.next.next.next; // 3 -> 5
+        head.next.next.next.rand = head.next.next; // 4 -> 3
+        head.next.next.next.next.rand = null; // 5 -> null
+        head.next.next.next.next.next.rand = head.next.next.next; // 6 -> 4
+
+        System.out.println("原始链表：");
+        printRandLinkedList(head);
+        System.out.println("=========================");
+        res1 = copy(head);
+        printRandLinkedList(res1);
+
+    }
+
+    public static Node copy(Node head) {
+        if (head == null) {
+            return null;
+        }
+        Node cur = head;
+        Node next = null;
+        // copy node and link to every node
+        // 1 -> 2
+        // 1 -> 1' -> 2
+        while (cur != null) {
+            // cur 老 next 老的下一个
+            next = cur.next;
+            cur.next = new Node(cur.value);
+            cur.next.next = next;
+            cur = next;
+        }
+        cur = head;
+        Node curCopy = null;
+        // set copy node rand
+        // 1 -> 1' -> 2 -> 2'
+        while (cur != null) {
+            // cur 老
+            // cur.next 新 copy
+            next = cur.next.next;
+            curCopy = cur.next;
+            curCopy.rand = cur.rand != null ? cur.rand.next : null;
+            cur = next;
+        }
+        // head head.next
+        Node res = head.next;
+        cur = head;
+        // split
+        while (cur != null) {
+            next = cur.next.next;
+            curCopy = cur.next;
+            cur.next = next;
+            curCopy.next = next != null ? next.next : null;
+            cur = next;
+        }
+        return res;
+    }
+
+    public static void printRandLinkedList(Node head) {
+        Node cur = head;
+        System.out.print("order: ");
+        while (cur != null) {
+            System.out.print(cur.value + " ");
+            cur = cur.next;
+        }
+        System.out.println();
+        cur = head;
+        System.out.print("rand:  ");
+        while (cur != null) {
+            System.out.print(cur.rand == null ? "- " : cur.rand.value + " ");
+            cur = cur.next;
+        }
+        System.out.println();
+    }
+}
