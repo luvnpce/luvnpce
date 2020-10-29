d2c27438cc1df254a7c277f279009d3e7f021c8b
luvnpce <dannyzoujx@gmail.com>
2020-10-29 13:43:53 -0700

    Kruskal

---
+++ algo/src/main/java/graph/Kruskal.java
@@ -0,0 +1,100 @@
+package graph;
+
+import graph.structure.Edge;
+import graph.structure.Graph;
+import graph.structure.Node;
+
+import java.util.*;
+
+/**
+ * 最小生成树算法之Kruskal
+ * - 总是从权值最小的Edge开始考虑，依次考察权值依次变大的Edge
+ * - 当前的Edge要么进入最小生成树的集合，要么丢弃
+ * - 如果当前的Edge进入最小生成树的集合中不会形成环，就要当前Edge
+ * - 如果当前的Edge进入最小生成树的集合中会形成环，就不要当前Edge
+ * - 考察完所有的Edge后，最小生成树的集合也得到了
+ *
+ * 代码解读
+ * 使用并查集，依次遍历Edge，判断Edge连接的两个Node是不是一个集合
+ * - 不是一个集合：把两个Node加在一起
+ * - 是一个集合：抛弃这个Edge
+ */
+public class Kruskal {
+
+    public static Set<Edge> kruskalMST(Graph graph) {
+        UnionFind unionFind = new UnionFind();
+        unionFind.makeSets(graph.nodes.values());
+        PriorityQueue<Edge> priorityQueue = new PriorityQueue<>((o1, o2) -> o1.weight - o2.weight);
+        for (Edge edge : graph.edges) { // M 条边
+            priorityQueue.add(edge);  // O(logM)
+        }
+        Set<Edge> result = new HashSet<>();
+        while (!priorityQueue.isEmpty()) { // M 条边
+            Edge edge = priorityQueue.poll(); // O(logM)
+            if (!unionFind.isSameSet(edge.from, edge.to)) { // O(1)
+                result.add(edge);
+                unionFind.union(edge.from, edge.to);
+            }
+        }
+        return result;
+    }
+
+    // 使用并查集
+    public static class UnionFind {
+        // key 某一个节点， value key节点往上的节点
+        private HashMap<Node, Node> fatherMap;
+        // key 某一个集合的代表节点, value key所在集合的节点个数
+        private HashMap<Node, Integer> sizeMap;
+
+        public UnionFind() {
+            fatherMap = new HashMap<Node, Node>();
+            sizeMap = new HashMap<Node, Integer>();
+        }
+
+        public void makeSets(Collection<Node> nodes) {
+            fatherMap.clear();
+            sizeMap.clear();
+            for (Node node : nodes) {
+                fatherMap.put(node, node);
+                sizeMap.put(node, 1);
+            }
+        }
+
+        private Node findFather(Node n) {
+            Stack<Node> path = new Stack<>();
+            while(n != fatherMap.get(n)) {
+                path.add(n);
+                n = fatherMap.get(n);
+            }
+            while(!path.isEmpty()) {
+                fatherMap.put(path.pop(), n);
+            }
+            return n;
+        }
+
+        public boolean isSameSet(Node a, Node b) {
+            return findFather(a) == findFather(b);
+        }
+
+        public void union(Node a, Node b) {
+            if (a == null || b == null) {
+                return;
+            }
+            Node aDai = findFather(a);
+            Node bDai = findFather(b);
+            if (aDai != bDai) {
+                int aSetSize = sizeMap.get(aDai);
+                int bSetSize = sizeMap.get(bDai);
+                if (aSetSize <= bSetSize) {
+                    fatherMap.put(aDai, bDai);
+                    sizeMap.put(bDai, aSetSize + bSetSize);
+                    sizeMap.remove(aDai);
+                } else {
+                    fatherMap.put(bDai, aDai);
+                    sizeMap.put(aDai, aSetSize + bSetSize);
+                    sizeMap.remove(bDai);
+                }
+            }
+        }
+    }
+}
