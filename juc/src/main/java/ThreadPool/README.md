ed4a3b23a3aec631113caf42dd3cb604b6799223
luvnpce <dannyzoujx@gmail.com>
2021-04-25 10:32:26 -0700

    threadpool

---
+++ juc/src/main/java/ThreadPool/README.md
@@ -0,0 +1,8 @@
+1. extends Thread
+    * override run()
+2. implements Runnable
+    * implements run()
+3. implements Callable<Integer> & Future
+    * override call()
+    * new FutureTask
+    
--- juc/src/main/java/ThreadPool/Test.java
+++ juc/src/main/java/ThreadPool/Test.java
@@ -1,20 +1,23 @@
 package ThreadPool;
 
-import java.util.LinkedList;
-import java.util.concurrent.*;
+public class Test implements Runnable{
 
-public class Test {
+    public void run() {
+        System.out.println("hello");
+    }
 
     public static void main(String[] args) throws InterruptedException {
-        ExecutorService ser = Executors.newCachedThreadPool();
-        ForkJoinPool forkJoinPool = new ForkJoinPool();
+//        ExecutorService ser = Executors.newCachedThreadPool();
+//        ForkJoinPool forkJoinPool = new ForkJoinPool();
+//
+//        BlockingQueue<String> queue = new ArrayBlockingQueue<>(10);
+//        queue.put("a");
+//
+//        ThreadPoolExecutor executor = new ThreadPoolExecutor(5, 10, 1000L, TimeUnit.SECONDS, new ArrayBlockingQueue<Runnable>(3));
+//
+//        LinkedList<String> list = new LinkedList<>();
+//        list.peekLast();
 
-        BlockingQueue<String> queue = new ArrayBlockingQueue<>(10);
-        queue.put("a");
-
-        ThreadPoolExecutor executor = new ThreadPoolExecutor(5, 10, 1000L, TimeUnit.SECONDS, new ArrayBlockingQueue<Runnable>(3));
-
-        LinkedList<String> list = new LinkedList<>();
-        list.peekLast();
+        new Thread(new Test(), "t1").start();
     }
 }
